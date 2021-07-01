3603b15a6ad60768560293f50f934957c52a4d2b
James Cross <jamescross808@gmail.com>
2021-06-30 22:58:41 -0400

    im an idiot and published my key

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 time = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
-DISCORD_TOKEN = 'NjU3MTE3MTQ5NjEyOTk4NjU3.XfsiCQ.fiD5uwIseat1kGbuxnaBEXio46Q'
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 
 discord_game_played = 'Runescape'
 
