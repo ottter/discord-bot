33997b9ffcbb71bb43241e8f590d07dfd613aef1
James Cross <jamescross808@gmail.com>
2021-07-05 14:59:28 -0400

    planning for custom command

--- modules/custom-command.py
+++ modules/custom-command.py
@@ -1,30 +1,47 @@
 from discord.ext import commands
 from discord import Game
-from admin import banned_users, bot_admins
 import discord
 import config
 import json
 
 
-class Aliases(commands.Cog):
-    """Basic bot admin-level controls"""
+def cc_add():
+    print('1 add')
+
+
+def cc_list():
+    print('2 list')
+
+
+def cc_delete():
+    print('3 delete')
+
+
+def cc_help():  # syntax help for creating a custom command
+    print('4 help')
+
+
+class CustomCommand(commands.Cog):
+    """Gives users the ability to add simple custom commands"""
 
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(alias='add_alias')
-    async def add_alias(self, context, user: discord.User, *alias):
-        user = str(context.message.author.id)
-        if
-        if str(context.message.author.id) in bot_admins or user.id == context.message.author.id:
-            if len(alias) > 1:
-                await context.send('The alias must be a single word.')
-                return
-
-            config.db['aliases'].update_one({'_id': user.id}, {'$addToSet': {'aliases': ' '.join(alias)}}, upsert=True)
-
-            await context.send('Successfully added alias.')
+    @commands.command()
+    async def cc(self, context):
+        args = context.message.content.lower().split(" ", 1)
+        if args[1] in ['add', 'a']:
+            # TODO: .cc add [name] content [content] .cc -a [name] -c [content]
+            cc_add()
+        elif args[1] in ['list', 'ls', 'l']:
+            cc_list()
+        elif args[1] in ['del', 'delete', 'rm', 'd']:
+            cc_delete()
+        elif args[1] in ['help', 'h']:
+            cc_help()
+        else:
+            print('invalid entry')
 
 
 def setup(bot):
-    bot.add_cog(Aliases(bot))
+    bot.add_cog(CustomCommand(bot))
