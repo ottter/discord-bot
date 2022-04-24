1503650848a27ee31058468069975ab6c6e24f65
james <me@jamescr.dev>
2022-04-24 16:19:18 -0400

    expanded wordle prevention

--- listeners/on_message.py
+++ listeners/on_message.py
@@ -1,18 +1,19 @@
 from discord.ext import commands
 from config import WORDLE_GLOBAL_BAN, WORDLE_BAN_LIST, PRIVATE_CHANNEL
 import discord
+import emoji
 import re
 
 
 rdleverse_dict = { 
-    "Wordle": "(Wordle )\d{1,}( )-?(([0]|[3-9]|\d{2,})|X)(\/(6|\d{3,}))", # Wordle 298 3/6
-    "Letterle": "(Letterle \d{1,}/26)",                                   # Letterle 7/26
-    "Heardle": "(#Heardle #)\d{1,}",                                      # #Heardle #47
-    "Dordle": "(Daily Dordle #\d{4} )((0-9|X)&(0-9|X)/7)",                # Daily Dordle #0078 X&X/7
-    "Tridle": "(TRIDLE - \d{1,}/3 words, (\d{1,}/8 tries|failed))",       # TRIDLE - 3/3 words, 7/8 tries
-    "Quordle": "(Daily Quordle \d{2,})",                                  # Daily Quordle 78
-    "Duotrigordle": "(Daily Duotrigordle #\d{2,})",                       # Daily Duotrigordle #42
-    "Lewdle": "(Lewdle 🍆💦 \d{2,})( (\d{1}|X)(/6))",                    # Lewdle 🍆💦 83 5/6
+    "Wordle": "(Wordle \d{1,} \d/\d)",                              # Wordle 298 3/6
+    "Letterle": "(Letterle \d{1,}/26)",                             # Letterle 7/26
+    "Heardle": "(#Heardle #)\d{1,}",                                # #Heardle #47
+    "Dordle": "(Daily Dordle #\d{4} )((0-9|X)&(0-9|X)/7)",          # Daily Dordle #0078 X&X/7
+    "Tridle": "(TRIDLE - \d{1,}/3 words, (\d{1,}/8 tries|failed))", # TRIDLE - 3/3 words, 7/8 tries
+    "Quordle": "(Daily Quordle \d{2,})",                            # Daily Quordle 78
+    "Duotrigordle": "(Daily Duotrigordle #\d{2,})",                 # Daily Duotrigordle #42
+    "Lewdle": "(Lewdle 🍆💦 \d{2,})( (\d{1}|X)(/6))",              # Lewdle 🍆💦 83 5/6
     }
 
 class OnMessage(commands.Cog):
@@ -22,6 +23,7 @@ class OnMessage(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, context):
         message = str(context.content.lower())
+
         if context.author == self.bot.user:
             return
 
@@ -36,8 +38,9 @@ class OnMessage(commands.Cog):
         # Moderates a wordler if either of the options are True by telling them to leave
         if (WORDLE_GLOBAL_BAN) or (context.channel.id in WORDLE_BAN_LIST) or (context.guild.id in WORDLE_BAN_LIST):
             for key, value in rdleverse_dict.items():
-                if re.search(value.lower(), message):
-                    print(f'Put a {key}r in their place')
+                square_count = len(re.findall("(_square:)", emoji.demojize(message)))
+                if re.search(value.lower(), message) or square_count > 10:
+                    print(f'Put a {key}r in their place ({context.author})')
                     return await context.channel.send(f"Get lost, {key}r")
 
         # Removed until I figure out why having this made commands run twice. Solution was add on_message event to main
