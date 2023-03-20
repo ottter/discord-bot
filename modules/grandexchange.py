c0c275c8bde74ff2587d3bd1b772510cf8474451
james <me@jamescr.dev>
2023-03-19 23:58:28 -0400

    works in print

--- modules/grandexchange.py
+++ modules/grandexchange.py
@@ -5,16 +5,17 @@ import requests
 import config
 
 # https://runescape.wiki/w/RuneScape:Grand_Exchange_Market_Watch/Usage_and_APIs
+# https://api.weirdgloop.org/
 
-def import_osrs_items():
-    url = "https://chisel.weirdgloop.org/gazproj/gazbot/rs_dump.json"
+def import_item(game, item):
+    base_url = f"https://api.weirdgloop.org/exchange/history/{game}/latest?name={item}"
     headers = {
         'User-Agent': 'github/ottter-discord-bot' }     # Owners of API request for a custom user-agent
-    response = requests.get(url=url, headers=headers)
-    print(f'RS Wiki Status: {response.status_code}')    # Good = 200
+    response = requests.get(url=base_url, headers=headers).json()
+    return response
 
-def search_osrs_items(item):
-    import_osrs_items()
+def search_grandexchange(item):
+    print(import_item(item))
 
 class GrandExchange(commands.Cog):
     """Provides user with random and relevant xkcd comics"""
@@ -24,12 +25,12 @@ class GrandExchange(commands.Cog):
     @commands.cooldown(1, 3, commands.BucketType.user)
     @commands.command()
     async def rsge(self, context):
-        return
+        return search_grandexchange('rs', 'cannonball')
 
     @commands.cooldown(1, 3, commands.BucketType.user)
     @commands.command(aliases=['07ge'])
     async def osge(self, context):
-        return search_osrs_items('cannonball')
+        return search_grandexchange('osrs', 'cannonball')
 
 
 def setup(bot):
