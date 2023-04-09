78b3cdb82c2a798a4b5314eb0fb7a2c557924f53
james <me@jamescr.dev>
2023-04-08 23:44:54 -0400

    refactor 8ball

--- modules/misc.py
+++ modules/misc.py
@@ -2,6 +2,14 @@
 from random import choice, randint
 from discord.ext import commands
 
+eightball_responses = [
+            'It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely',
+            'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good',
+            'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later',
+            'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
+            'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good',
+            'Very doubtful']
+
 
 class Misc(commands.Cog):
     """Miscellaneous commands that did not fit in anywhere else"""
@@ -12,14 +20,7 @@ class Misc(commands.Cog):
     @commands.command(name='8ball', pass_context=True)
     async def eight_ball(self, context):
         """Ask the magic 8 ball any question"""
-        possible_responses = [
-            'It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely',
-            'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good',
-            'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later',
-            'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
-            'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good',
-            'Very doubtful']
-        await context.send(f"🎱 {choice(possible_responses)}, {context.message.author.mention} 🎱")
+        await context.send(f"🎱 {choice(eightball_responses)}, {context.message.author.mention} 🎱")
 
     @commands.cooldown(1, 2, commands.BucketType.user)
     @commands.command(name='roll', aliases=['dice'], pass_context=True)
